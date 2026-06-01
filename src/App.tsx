8f5e550539ea332637bad4b8a5220ddb77bcc6db
Hwangbohye <hwangbohye98@gmail.com>
2026-06-01 15:42:49 +0900

    feat: 인증 관련 페이지 라우트 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import HomePage from './pages/HomePage'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import LoginPage from './pages/auth/desktop/LoginPage.tsx'
+import SignupPage from './pages/auth/desktop/SignupPage.tsx'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      {/* auth 관련 페이지 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* home 관련 페이지 */}
+      <Route path="/" element={<HomePage />} />
+
+    </Routes>
+  )
 }
 
 export default App
